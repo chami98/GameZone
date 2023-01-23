04633abca35f9d10ee33116579ca3df8d633eb4e
chami98 <chamikara98@gmail.com>
2023-01-23 14:58:03 +0530

    Added expo apploader to asynchrnouslty fetch the fonts to the app

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import * as Font from 'expo-font'
 import Home from './screens/home';
 import About from './screens/About'
+import { useState } from 'react';
+import AppLoading from 'expo-app-loading';
 
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+})
 
 export default function App() {
-  return (
 
-    <Home />
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    )
+  }
+
+
 }
 
