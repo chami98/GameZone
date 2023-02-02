6fe2c1d319935bbcf6e1f5228d51668eca3783cc
chami98 <chamikara98@gmail.com>
2023-02-02 21:54:50 +0530

    created a button to open drawer navigation and created a custome header

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,14 +1,16 @@
 import { createStackNavigator } from "react-navigation-stack";
 import Home from "../screens/Home";
 import ReviewDetails from "../screens/ReviewDetails"
+import Header from '../shared/header'
+import React from 'react';
+
 
 const screens = {
     Home: {
         screen: Home,
-        navigationOptions: {
-            title: 'GameZone',
-            headerStyle: {
-                backgroundColor: '#333'
+        navigationOptions: ({ navigation }) => {
+            return {
+                headerTitle: () => <Header navigation={navigation} title='GameZone' />
             }
         }
     },
