fa848c61fcf9649bf4d6f6f23af913fd1176c14d
chami98 <chamikara98@gmail.com>
2023-02-02 20:13:29 +0530

    Passed data between screen using navigation.navigate method and fetch data using navigation.getParams method

--- App.js
+++ App.js
@@ -5,28 +5,34 @@ import About from './screens/About'
 import { useState } from 'react';
 import AppLoading from 'expo-app-loading';
 import Navigator from "./routes/homeStack"
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import { View } from 'react-native-web';
+import { globalStyles } from './styles/global';
 
 const getFonts = () => Font.loadAsync({
   'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
   'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
 })
 
+const [reviews, setReviews] = useState([
+  { title: 'Zelda, Breth of Fresh Air1', rating: '5', body: 'lorem ipsum', key: '1' },
+  { title: 'Zelda, Breth of Fresh Air2', rating: '5', body: 'lorem ipsum', key: '2' },
+  { title: 'Zelda, Breth of Fresh Air3', rating: '5', body: 'lorem ipsum', key: '3' },
+]);
+
 export default function App() {
 
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-  if (fontsLoaded) {
-    return (
-      <Home />
-    );
-  } else {
-    return (
-      <AppLoading
-        startAsync={getFonts}
-        onFinish={() => setFontsLoaded(true)}
-        onError={console.warn}
-      />
-    )
-  }
+  <View style={globalStyles.container}>
+    <FlatList
+      data={reviews}
+      renderItem={(item) => (
+        <TouchableOpacity onPress={() => navigator.navigate('ReviewDetails', item)}>
+          <Text style={globalStyles.titleText}>{item.title}</Text>
+        </TouchableOpacity>
+      )}
+
+    />
+  </View>
 
 
 }
