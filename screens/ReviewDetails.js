065f4a5b226a349ed1b59ec34dc7b8538388b7f0
chami98 <chamikara98@gmail.com>
2023-02-03 21:37:34 +0530

    Added rating images dynamically

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native'
+import { StyleSheet, View, Text, Image } from 'react-native'
 import Card from '../shared/card';
+import { images } from '../styles/global';
 
 export default function ReviewDetails({ navigation }) {
 
+    const reating = navigation.getParam('rating');
 
 
     return (
@@ -11,7 +13,13 @@ export default function ReviewDetails({ navigation }) {
             <Card>
                 <Text>{navigation.getParam('title')}</Text>
                 <Text>{navigation.getParam('body')}</Text>
-                <Text>{navigation.getParam('rating')}</Text>
+                <View style={styles.rating}>
+                    <Text>GameZone rating</Text>
+                    <Image
+                        source={images.ratings[rating]}
+                    />
+                </View>
+
             </Card>
         </View>
     )
@@ -20,6 +28,15 @@ export default function ReviewDetails({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         padding: 24
+    },
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
     }
+
 });
 
