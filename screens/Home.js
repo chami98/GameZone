8349e7083d02352ef0c4c61f1dee8fe483385b19
chami98 <chamikara98@gmail.com>
2023-02-04 22:29:21 +0530

    added modal to add reviews

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text, Button, TouchableOpacity, Modal } from 'react-native'
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
+import { MaterialIcons } from '.@expo/vector-icons';
 import { FlatList } from 'react-native-gesture-handler';
 
 
@@ -9,12 +10,34 @@ import { FlatList } from 'react-native-gesture-handler';
 
 export default function Home({ navigation }) {
 
+    const [modalOpen, setModalOpen] = useState(false)
+
     const pressHandler = () => {
         navigation.navigate('ReviewDetails');
     }
 
     return (
         <View style={globalStyles.container}>
+
+            <Modal visible={modalOpen} animationType='slide'>
+                <View style={styles.modalContent}>
+                    <MaterialIcons
+                        name='close'
+                        size={24}
+                        onPress={() => setModalOpen(false)}
+                        style={{ ...styles.modalToggle, ...styles.modalClose }}
+                    />
+                    <Text>Hello form the modal</Text>
+                </View>
+            </Modal>
+
+            <MaterialIcons
+                name='add'
+                size={24}
+                onPress={() => setModalOpen(true)}
+                style={styles.modalToggle}
+            />
+
             <FlatList
                 data={reviews}
                 renderItem={({ item }) => (
@@ -35,6 +58,21 @@ const styles = StyleSheet.create({
     }, titleText: {
         fontFamily: 'nunito-bold',
         fontSize: 18
+    },
+    modalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center'
+    },
+    modalClose: {
+        margin: 20,
+        marginBottom: 0,
+    },
+    modalContent: {
+        flex: 1
     }
 });
 
